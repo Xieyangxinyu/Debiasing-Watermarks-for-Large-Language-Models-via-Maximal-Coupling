eaf15b74f216174a88b72741144c928767f4d477
Yangxinyu Xie <xinyux@wharton.upenn.edu>
2024-11-17 13:11:45 -0500

    Update __init__.py

--- wm/__init__.py
+++ wm/__init__.py
@@ -1,9 +1,3 @@
-"""
-Author: Yangxinyu Xie
-Date: 2024-11-07
-
-"""
-
 from .generator import WmGenerator, OpenaiGenerator, MarylandGenerator, DiPMarkGenerator
 from .detector import WmDetector, OpenaiDetector, MarylandDetector, DiPMarkDetector
 from .coupling import CouplingGenerator, CouplingGeneratorOneList, CouplingSumDetector, CouplingSumDetectorOneList, CouplingMaxDetector, CouplingHCDetector, CouplingHCDetectorOneList
